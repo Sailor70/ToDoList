4e62166867ebe5d47a83c3f258605d3a62cf3344
Sailor70 <grzywa96@gmail.com>
2021-05-31 21:21:03 +0200

    TaskAdapter onClick() working

--- app/src/main/java/com/pawelchmielarski/todolist/TaskAdapter.java
+++ app/src/main/java/com/pawelchmielarski/todolist/TaskAdapter.java
@@ -2,18 +2,23 @@ package com.pawelchmielarski.todolist;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class TaskAdapter extends ArrayAdapter<Task> {
 
+    Context context;
     public TaskAdapter(Context context, ArrayList<Task> tasks) {
+
         super(context, 0, tasks);
+        this.context = context;
     }
 
     @Override
@@ -32,6 +37,27 @@ public class TaskAdapter extends ArrayAdapter<Task> {
         // Populate the data into the template view using the data object
         tvName.setText(task.getName());
         tvDeadline.setText(task.getDeadline().toString());
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+//                if(view.)  // reakcje na klikanie poszczególnych elementów itemu na liście (
+
+                Intent i=new Intent(context, TaskDetailsActivity.class);
+
+//                i.putExtra(task);
+                context.startActivity(i);
+
+                Toast.makeText(context, "kliknięto task o pozycji: " + task.getName(), Toast.LENGTH_LONG).show();
+//                User user = getItem(position);
+
+                // Do what you want here...
+
+            }
+
+        });
         // Return the completed view to render on screen
 
         return convertView;
